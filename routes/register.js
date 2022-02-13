b654b387ac98739637785e3678e984710f5906f5
NikaKiria <knika508@gmail.com>
2022-02-13 15:11:15 +0400

    fix register route

--- routes/register.js
+++ routes/register.js
@@ -6,7 +6,7 @@ const User = require('../models/User.js');
 const router = express.Router();
 
 // Schema for validation new user info
-const newUserSchema = joi.object({
+const newUserSchema = joi.object().keys({
     username: joi.string()
         .alphanum()
         .min(3)
@@ -26,17 +26,34 @@ const newUserSchema = joi.object({
 // Post request to register user
 router.post('/register', async (req,res) => {
     try{
+        // Validate User info
         const newUserInfo = await req.body;
         const validatedInfo = newUserSchema.validate(newUserInfo);
         if(validatedInfo.error){
-            console.log(validatedInfo.error);
-            res.status(400).json({"vaidator error":validatedInfo.error});
+            return res.status(400).json({"Validator error":validatedInfo.error});
         }
-        //FIXME: Check if user already exist and register
+        // Check if user already exist
+        const alreadyRegistered = await User.findOne({username: newUserInfo.username});
+        if(alreadyRegistered){
+            console.log("User is already  registered I swear");
+            return res.status(409).json("User Already Registered");
+        }
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        newUserInfo.password = await bcrypt.hash(newUserInfo.password, salt);
+        newUserInfo.repeat_password =  await bcrypt.hash(newUserInfo.repeat_password, salt);
+        // Create new user
+        const newUser = await new User(newUserInfo);
+        newUser.save((err) => {
+            if(err){
+                return res.status(500).json("Server Error!");
+            }else{
+                return res.status(201).json("User Registered!");
+            }
+        });
     } catch(err) {
         console.log(err);
     }
-
 });
 
 module.exports = router;
