f7b920e5df439e3029a8b50b9562b77c36edd412
NikaKiria <knika508@gmail.com>
2022-02-15 20:18:17 +0400

    Create auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jwt');
 
-const generateAccessToken = (email, secretToken) => {
-    return jwt.sign({email}, secretToken , {expiresIn: "1d"});
+const auth = (req,res,next) => {
+    try{
+        // Checking if token exists
+        const token = req.header("x-auth-token");
+        if(!token){
+            return res.status(401).json("Authorisation Denied!");
+        }
+        // Verifying Token
+        const verified = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+        if(!verified){
+            return res.status(401).json("Authorisation Denied!");
+        }
+        req.user = verified.id;
+        next();
+    }catch(err) {
+        res.status(500).json("Server Error");
+    }
 };
-
-module.exports = generateAccessToken;
-  
