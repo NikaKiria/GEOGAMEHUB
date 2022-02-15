ed6774dc1f7ee36c6760b7a5fe46263da1943100
NikaKiria <knika508@gmail.com>
2022-02-15 20:16:04 +0400

    add mongoose connect options

--- database/database.js
+++ database/database.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 
 // Connect to Database
 const connectToDB = (MONGO_URI) => {
-    mongoose.connect(MONGO_URI, () => {
-        console.log("connected to database");
-    });
+    mongoose.connect(MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        (err) => {
+            if(err){
+                throw err;
+            }else{
+                console.log("connected to database");
+            }
+        });
 };
 
 
