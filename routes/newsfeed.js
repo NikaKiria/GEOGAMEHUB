0339c143e9057923aa3665d056435fb63e3e84a7
NikaKiria <knika508@gmail.com>
2022-02-17 20:32:16 +0400

    create new route to get posts for newsfeed

---
+++ routes/newsfeed.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const auth = require('../middlewares/auth.js');
+const Post = require('../models/Post.js');
+const router = express.Router();
+
+// Get Posts for newsfeed
+router.get('/posts', auth, async (req,res) => {
+    try{
+        const posts = await Post.find().limit(100);
+        if(posts){
+            res.status(200).json(posts);
+        }else{
+            res.status(404).json("Cant get posts");
+        }
+    }catch(err) {
+        console.log(err);
+    }
+    
+});
+
+module.exports = router;
