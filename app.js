313206f48c360207c6c25dcc7a8a87447b836d86
NikaKiria <knika508@gmail.com>
2022-03-11 19:37:35 +0400

    fix h10 error on heroku by changing port

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/api/v1', deletePostRoute);
 app.use('/api/v1', deleteComment);
 
 // Listen to port
-app.listen(process.env.PORT || 8000, () => {
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
     console.log(`Listening to port: ${port}`);
 });
